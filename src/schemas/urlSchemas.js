eb7efbcf4ceca960d1221afdd19b74be845a9230
layconjohn <layconjohn@gmail.com>
2022-10-14 16:33:36 -0300

    feat: O link para ser encurtado deve ser válido

--- src/schemas/urlSchemas.js
+++ src/schemas/urlSchemas.js
@@ -1,8 +1,9 @@
 import joi from "joi";
 
-const regex = "((https?:\/\/)|(ftp:\/\/)|(^))([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+([a-zA-Z]{2,9})(:\d{1,4})?([-\w\/#~:.?+=&%@~]*)";
+//const regex = "((https?:\/\/)|(ftp:\/\/)|(^))([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+([a-zA-Z]{2,9})(:\d{1,4})?([-\w\/#~:.?+=&%@~]*)";
 const urlSchema = joi.object({
-    url: joi.string().pattern(new RegExp(regex)).required()
+    url: joi.string().uri().required()
 })
+//url: joi.string().pattern(new RegExp(regex)).required()
 
 export {urlSchema};
