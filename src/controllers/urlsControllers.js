3caaf63b6d6979b7270dddc2088cfa162d635c57
layconjohn <layconjohn@gmail.com>
2022-10-14 21:24:56 -0300

    feat: rota GET /urls/open/:shortUrl

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -16,14 +16,16 @@ async function shortenUrl(req, res) {
     if (!token) {
         return res.sendStatus(401);
     }
-    const urlShort = nanoid(10);
+    const shortUrl = nanoid(10);
     try {
         const session = (await db.query('SELECT * FROM sessions WHERE token = $1', [token])).rows[0];
         if (!session) {
             return res.sendStatus(401);
         }
 
-        return res.status(201).send({shortUrl: `${process.env.DOMAIN}/${urlShort}`});
+        await db.query('INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3)', [url, shortUrl, session.userId]);
+
+        return res.status(201).send({shortUrl: `${shortUrl}`});
     } catch (error) {
         console.error(error.message);
         return res.sendStatus(500);
@@ -31,4 +33,44 @@ async function shortenUrl(req, res) {
     
 }
 
-export {shortenUrl};
+async function getUrlById(req, res) {
+    const {id} = req.params;
+    if (!id) {
+        return res.sendStatus(404);
+    }
+    try {
+        const url = (await db.query('SELECT id, "shortUrl", url FROM urls WHERE id = $1', [id])).rows[0];
+        if (!url) {
+            return res.sendStatus(404);
+        }
+        
+        return res.status(200).send(url);
+    } catch (error) {
+        console.error(error.message);
+        return res.sendStatus(500);
+    }
+}
+
+async function redirectUrl(req, res) {
+    const {shortUrl} = req.params;
+
+    try {
+        const url = (await db.query('SELECT * FROM urls WHERE "shortUrl" = $1', [shortUrl])).rows[0];
+        if (!url) {
+            return res.sendStatus(404);
+        }
+
+        await db.query('UPDATE urls SET "visitCount" = $1 WHERE id = $2', [(url.visitCount + 1), url.id]);
+        console.log(url);
+        return res.redirect(url.url);
+    } catch (error) {
+        console.error(error.message);
+        return res.sendStatus(500);
+    }
+}
+
+export {
+    shortenUrl, 
+    getUrlById,
+    redirectUrl
+};
--- src/routers/urlsRouters.js
+++ src/routers/urlsRouters.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { shortenUrl } from "../controllers/urlsControllers.js";
+import { shortenUrl, getUrlById, redirectUrl } from "../controllers/urlsControllers.js";
 
 const router = Router();
 
 router.post("/urls/shorten", shortenUrl);
+router.get("/urls/:id", getUrlById);
+router.get("/urls/open/:shortUrl", redirectUrl);
 
 export default router;
